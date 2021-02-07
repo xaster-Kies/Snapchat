dad286eafb05195fdca0ba75ff956c2f30c37e12
TiokengSamuel <samueledward40@gmail.com>
2021-02-07 19:12:30 +0100

    config of firebase

--- xsnapchat/src/Firebase.js
+++ xsnapchat/src/Firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyAZ2KHdjXDJ7YguyDssVgYh-YKmwJjdDsw",
     authDomain: "xsnapchat-d682a.firebaseapp.com",
@@ -7,3 +9,11 @@ const firebaseConfig = {
     appId: "1:708943079971:web:0204422d7bfe75f510df96",
     measurementId: "G-S5T3Z7HD2H"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth =  firebase.auth();
+  const storage = firebase.storage();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { db, auth, storage, provider };

249fa7981eab044333499a43f6f230d71dfab5e9
TiokengSamuel <samueledward40@gmail.com>
2021-02-08 10:53:26 +0100

    icons

--- xsnapchat/src/Preview.js
+++ xsnapchat/src/Preview.js
@@ -3,6 +3,14 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom';
 import { resetCameraImage, selectcameraImage } from './features/cameraSlice'
 import CloseIcon from '@material-ui/icons/Close'
+import TextFieldIcon from '@material-ui/icons/TextFields'
+import CreateIcon from '@material-ui/icons/Create'
+import NoteIcon from '@material-ui/icons/Note'
+import MusicIcon from '@material-ui/icons/Misic'
+import AttachFileIcon from '@material-ui/icons/AttachFile'
+import TimerIcon from '@material-ui/icons/Timer'
+import CropIcon from '@material-ui/icons/Crop'
+
 import "./Preview.css"
 
 function Preview() {
@@ -25,10 +33,13 @@ function Preview() {
         <div className="preview">
             <CloseIcon onClick={closePreview} className='preview__close'/>
             <div className="preview__toolbarRight">
-                <TextFieldIcons/>
+                <TextFieldIcon/>
                 <CreateIcon/>
                 <NoteIcon/>
-                
+                <MusicNoteIcon/>
+                <AttachFileIcon/>
+                <CropIcon/>
+                <TimerIcon/>
             </div>
             <img src={cameraImage} alt=""/>
         </div>

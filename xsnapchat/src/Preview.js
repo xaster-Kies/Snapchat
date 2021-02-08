b22e96a5ee5cc20a472cdfd0f677b786e35fade1
TiokengSamuel <samueledward40@gmail.com>
2021-02-08 10:27:35 +0100

    history import

--- xsnapchat/src/Preview.js
+++ xsnapchat/src/Preview.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom';
 import { selectcameraImage } from './features/cameraSlice'
 import "./Preview.css"
 
 function Preview() {
     const cameraImage = useSelector(selectcameraImage);
+    const history = useHistory();
 
     useEffect(() => {
         if(!cameraImage) {
-            history
+            history.replaceState("/")
         }
-    })
+    }, [cameraImage, history]);
 
     return (
         <div className="preview">

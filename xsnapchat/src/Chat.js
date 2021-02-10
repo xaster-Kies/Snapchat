a490692d0f1ccd1ca50bcee970d1ffc75368dfff
TiokengSamuel <samueledward40@gmail.com>
2021-02-10 16:30:52 +0100

    styling chats

--- xsnapchat/src/Chat.js
+++ xsnapchat/src/Chat.js
@@ -1,10 +1,21 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './Chat.css'
+import SearchIcon from "@materail-ui/icons/Search"
+import ChatBubble from "@materail-ui/icons/ChatBubble"
+
 
 function Chat() {
     return (
         <div class="chats">
-            
+            <div className="chats__header">
+                <Avatar className="chats__avatar"/>
+                <div className="chats__search">
+                    <SearchIcon/>
+                    <input placeholder="Friends" type="text"/>
+                    <ChatBubbleIcon className="chats__chatsIcons" />
+                </div>
+            </div>
         </div>
     )
 }
